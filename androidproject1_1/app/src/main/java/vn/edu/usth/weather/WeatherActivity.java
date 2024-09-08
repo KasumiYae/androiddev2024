ec9b929983fbe066a2e4e4407dc527fb11ca55a2
KasumiYae <kiriha0205@gmail.com>
2024-09-09 00:01:43 +0700

    practice 6

--- androidproject1_1/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ androidproject1_1/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,60 +3,60 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private static final String TAG = "FunctionTracing";
+    private static final String TAG = "WeatherActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather);
+
+        WeatherFragment weatherFragment = new WeatherFragment();
+        getSupportFragmentManager().findFragmentById(R.id.weatherFragment);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.weatherFragment, weatherFragment);
+        transaction.commit();
 
         ForecastFragment forecastFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.main, forecastFragment).commit();
+        getSupportFragmentManager().findFragmentById(R.id.forecastFragment);
+        FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+        transaction1.add(R.id.forecastFragment, forecastFragment);
+        transaction1.commit();
 
-        setContentView(R.layout.activity_weather2);
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-
-
+        Log.i(TAG, "onCreate");
     }
+
     @Override
-    protected void onStart(){
-        Log.i(TAG,"my app start");
+    protected void onStart() {
         super.onStart();
+        Log.i(TAG, "Start");
     }
 
     @Override
-    protected void onResume(){
-        Log.i(TAG,"my app resume");
+    protected void onResume() {
         super.onResume();
+        Log.i(TAG, "Resume");
     }
 
     @Override
-    protected void onPause(){
-        Log.i(TAG,"my app pause");
+    protected void onPause() {
         super.onPause();
+        Log.i(TAG, "Pause");
     }
 
     @Override
-    protected void onStop(){
-        Log.i(TAG,"my app stop");
+    protected void onStop() {
+        Log.i(TAG, "Stop");
         super.onStop();
     }
 
     @Override
-    protected void onDestroy(){
-        Log.i(TAG,"my app destroy");
-        super.onDestroy();}
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroy");
+    }
 }
